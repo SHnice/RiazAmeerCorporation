96767fb238dbf24f2486382d5014df657b583863
Saddam Hussain <108930470+SHnice@users.noreply.github.com>
2023-05-12 04:36:44 +0500

    Update OneCategoryBox.js

--- components/homepage/OneCategoryBox.js
+++ components/homepage/OneCategoryBox.js
@@ -7,7 +7,7 @@ export default function OneCategoryBox(props) {
     <div class=" -m-4">
       <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
         <a class="block relative h-12 w-20 md:h-24 md:w-48 rounded">
-          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260"/>
+          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src={props.imageUrl}/>
           <h3 class="text-gray-500 text-xs mt-1 text-center tracking-widest title-font">{props.categoryName}</h3>
         </a>
    
